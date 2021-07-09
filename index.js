44d19663e2a9d5b9ac2b63fa96fd68ee37747024
Devang <devanggandhi2222@gmail.com>
2021-07-10 01:19:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,32 @@ function myFunction() {
      theme.href = "index1.css";
    }
  });
+ function menufun(){
+  var x = document.getElementById("nav");
+  if(x.style.width === "0px"){
+      x.style.width = "100%";
+  }
+  else{
+      x.style.width = "0px"
+  }
+}
+function menufun1(){
+   var y = document.getElementById("nav2");
+   if(y.style.left === "-1000px"){
+       y.style.left = "0px";
+       y.style.top === "-1000px";
+       y.style.top = "0px"
+   }
+   var z = document.getElementById("open");
+   if(z.innerHTML === "MENU"){
+       z.innerHTML = "CLOSE";
+       z.style.backgroundColor === "#454545";
+       z.style.backgroundColor = "#cc6424";
+   }
+   else{
+       y.style.left = "-1000px";
+       y.style.top = "-1000px";
+       z.innerHTML = "MENU";
+       z.style.backgroundColor = "#454545";
+   }
+}

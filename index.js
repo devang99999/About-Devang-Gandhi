ecf3892382e831f44db7f2731e6e7e8ed874aa65
Devang <devanggandhi2222@gmail.com>
2022-01-04 21:43:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,20 @@
-function myFunction() {
+function darkmode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    document.getElementById("black").classList.toggle("navdar")
  }
- // Select the button
- const btn = document.querySelector(".btn-toggle");
- // Select the stylesheet <link>
- const theme = document.querySelector("#theme-link");
- 
- // Listen for a click on the button
- btn.addEventListener("click", function() {
-   // If the current URL contains "ligh-theme.css"
-   if (theme.getAttribute("href") == "index1.css") {
-     // ... then switch it to "dark-theme.css"
-     theme.href = "index1.css ";
-   // Otherwise...
-   } else {
-     // ... switch it to "light-theme.css"
-     theme.href = "index1.css";
-   }
- });
- function menufun(){
-  var x = document.getElementById("nav");
-  if(x.style.width === "0px"){
-      x.style.width = "100%";
-  }
-  else{
-      x.style.width = "0px"
-  }
-}
-function menufun1(){
-   var y = document.getElementById("nav2");
-   if(y.style.left === "-1000px"){
-       y.style.left = "0px";
-       y.style.top === "-1000px";
-       y.style.top = "0px"
-   }
-   var z = document.getElementById("open");
-   if(z.innerHTML === "MENU"){
-       z.innerHTML = "CLOSE";
-       z.style.backgroundColor === "#454545";
-       z.style.backgroundColor = "#cc6424";
+
+function myFunction3() {
+  document.getElementById("nav").classList.toggle("navd")
+  document.getElementById("nav2").classList.toggle("nav2d")
+  var z = document.getElementById("open");
+   if(z.innerHTML === "CLOSE"){
+       z.innerHTML = "MENU";
+       z.style.backgroundColor === "#cc6224";
+       z.style.backgroundColor = "#454545";
    }
    else{
-       y.style.left = "-1000px";
-       y.style.top = "-1000px";
-       z.innerHTML = "MENU";
-       z.style.backgroundColor = "#454545";
+           z.innerHTML = "CLOSE";
+           z.style.backgroundColor = "#cc6224";
    }
 }

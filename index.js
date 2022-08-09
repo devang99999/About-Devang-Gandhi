4bf635c317dbac0acb4c9cc936d0cfb307d3a64f
Devang <devanggandhi2222@gmail.com>
2022-08-09 11:30:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,5 +22,7 @@ function myFunction3() {
    }
 }
 function myFunction() {
+    document.title = "taking to the next level";
+    document.write(<h1>"taking to my new website"</h1>)
     location.replace("https://webdevang.netlify.app")
   }
